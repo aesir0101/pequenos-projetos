c062f817c55a7ebaf864d60a7f09e3dfb5ed5750
boromir08 <84733192+Dantes008@users.noreply.github.com>
2022-05-26 17:22:08 -0300

    Update main.js.js

--- countdown timer/main.js.js
+++ countdown timer/main.js.js
@@ -34,7 +34,7 @@ const contagemRegressiva = (tempo) => {
 
 const tempoRestante = () => {
     // 1 de janeiro de 1970
-    const dataEvento = new Date ('2020-10-26 20:00:00');
+    const dataEvento = new Date ('2024-10-26 20:00:00');
     const hoje = Date.now();
     return Math.floor((dataEvento - hoje) / 1000);
 }
